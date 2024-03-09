96581d76ef3149ff9061486cc17c60dbc5625f47
srhill12 <stevenhill@stevens-mbp.lan>
2024-03-08 23:41:32 -0600

    updated modules and main function

--- customer_banking.py
+++ customer_banking.py
@@ -1,6 +1,7 @@
 # Import the create_cd_account and create_savings_account functions
 # ADD YOUR CODE HERE
-
+from cd_account import create_cd_account
+from savings_account import create_savings_account
 # Define the main function
 def main():
     """This function prompts the user to enter the savings and cd account balance, interest rate,
@@ -9,21 +10,29 @@ def main():
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     # ADD YOUR CODE HERE
-
+    savings_balance = float(input("Enter savings balance: "))
+    savings_interest = float(input("Enter savings interest rate: "))
+    savings_maturity = int(input("Enter number of months: "))
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
-
+    print(f"Savings Interest Earned: {interest_earned}") # values need to be formatted 2 decimal places
+    print(f"Updated Balance: {updated_savings_balance}")
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
+    cd_balance = float(input("Enter CD Balance: "))
+    cd_interest = float(input("Enter CD Interest Rate: "))
+    cd_maturity = int(input("Enter number of months: "))
 
     # Call the create_cd_account function and pass the variables from the user.
     updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
-
+    print(f"CD Interest Earned: {interest_earned}") #does it matter this variable named same as previous?
+    print(f"Updated CD Balance: {updated_cd_balance}") #values need to be formatted 2 decimal places
 if __name__ == "__main__":
     # Call the main function.
+    main()
